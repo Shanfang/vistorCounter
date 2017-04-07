9be69a84b575a09dcf8a92cd26d04f30e1825562
Shanfang <shanfangzhao@gmail.com>
2017-04-07 09:03:12 -0400

    Fix an syntax error
    Error in line 3

---
+++ counter.js
@@ -0,0 +1,14 @@
+var = require('http');
+
+var = userCount = 0;
+http.createServer(function (request, response) {
+	console.log('New connection, Go gators!');
+	userCount++;
+
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.write('Hello!\n');
+	response.write('There are ' + userCount + 'vistors!\n');
+	response.end();
+}).listen(8080);
+
+console.log('Server started...');
