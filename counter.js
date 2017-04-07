f396915c66eca05ac10397925feb2463a60b3dbb
Shanfang <shanfangzhao@gmail.com>
2017-04-07 09:29:38 -0400

    Typo and syntax error
    Oops, typo and syntax error..

--- counter.js
+++ counter.js
@@ -1,13 +1,13 @@
-var = require('http');
+var http = require('http');
 
-var = userCount = 0;
+var userCount = 0;
 http.createServer(function (request, response) {
-	console.log('New connection, Go gators!');
+	console.log('New connection, go gators!');
 	userCount++;
 
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.write('Hello!\n');
-	response.write('There are ' + userCount + 'vistors!\n');
+	response.write('There are ' + userCount + ' vistors!\n');
 	response.end();
 }).listen(8080);
 
